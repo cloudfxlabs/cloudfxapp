f1a8a24c29d81105e6a1ec22c5e831241b3c1314
vishnucfx <vgunipati@cloudfxlabs.io>
2017-03-24 20:23:00 +0530

    newone

---
+++ mail.php
@@ -0,0 +1,39 @@
+<!-- <?php
+$to = '';
+$subject = '';
+$from = 'vvishnu405@gmail.com';
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Create email headers
+$headers .= 'From: '.$user_email."\r\n".
+    'Reply-To: '.$user_email."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+// Compose a simple HTML email message
+$message = '<html><body>';
+$message .= '<h1 style="color:#f40;">Hi Jane!</h1>';
+$message .= '<p style="color:#080;font-size:18px;">Will you marry me?</p>';
+$message .= '</body></html>';
+
+// Sending email
+if(mail($to, $subject, $message, $headers)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
+?> -->
+
+
+<?php
+$to = 'recipients@email-address.com';
+$subject = 'Hello from XAMPP!';
+$message = 'This is a test';
+$headers = "From: vvishnu405@gmail.com\r\n";
+if (mail($to, $subject, $message, $headers)) {
+ echo "SUCCESS";
+} else {
+ echo "ERROR";
+}
