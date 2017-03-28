c1d2a97f666f34d86bbc59dc8ead314acbb62673
vishnucfx <vgunipati@cloudfxlabs.io>
2017-03-28 20:31:48 +0530

    adding latest code

--- mail.php
+++ mail.php
@@ -1,39 +1,21 @@
-<!-- <?php
-$to = '';
-$subject = '';
-$from = 'vvishnu405@gmail.com';
-
-// To send HTML mail, the Content-type header must be set
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-// Create email headers
-$headers .= 'From: '.$user_email."\r\n".
-    'Reply-To: '.$user_email."\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-// Compose a simple HTML email message
-$message = '<html><body>';
-$message .= '<h1 style="color:#f40;">Hi Jane!</h1>';
-$message .= '<p style="color:#080;font-size:18px;">Will you marry me?</p>';
-$message .= '</body></html>';
-
-// Sending email
-if(mail($to, $subject, $message, $headers)){
-    echo 'Your mail has been sent successfully.';
-} else{
-    echo 'Unable to send email. Please try again.';
-}
-?> -->
-
-
 <?php
-$to = 'recipients@email-address.com';
-$subject = 'Hello from XAMPP!';
-$message = 'This is a test';
-$headers = "From: vvishnu405@gmail.com\r\n";
-if (mail($to, $subject, $message, $headers)) {
- echo "SUCCESS";
-} else {
- echo "ERROR";
-}
+$to = 'InsertYourEmailAddress';
+$subject = "Beautiful HTML Email using PHP by CodexWorld";
+// Get HTML contents from file
+$htmlContent = file_get_contents("email_template.html");
+
+// Set content-type for sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// Additional headers
+$headers .= 'From: vvishnu405@gmail.com.com>' . "\r\n";
+// $headers .= 'Cc: codexworld@gmail.com' . "\r\n";
+
+// Send email
+if(mail($to,$subject,$htmlContent,$headers)):
+	$successMsg = 'Email has sent successfully.';
+else:
+	$errorMsg = 'Some problem occurred, please try again.';
+endif;
+?>
