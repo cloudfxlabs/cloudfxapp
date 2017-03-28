ba1758041e3beaa6d0a867c5ad12e8f964614d7d
vishnucfx <vgunipati@cloudfxlabs.io>
2017-03-28 20:51:17 +0530

    merg

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 $to = 'InsertYourEmailAddress';
 $subject = "Beautiful HTML Email using PHP by CodexWorld";
 // Get HTML contents from file
-$htmlContent = file_get_contents("email_template.html");
+$htmlContent = file_get_contents("contact.html");
 
 // Set content-type for sending HTML email
 $headers = "MIME-Version: 1.0" . "\r\n";
