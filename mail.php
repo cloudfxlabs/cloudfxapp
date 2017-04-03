4209aab17dcb92fa412c8160d39a5a9f0a596c3f
vishnucfx <vgunipati@cloudfxlabs.io>
2017-04-03 18:19:25 +0530

    mail

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 
      <?php
-        $to = "vgunipati@cloudfxlabs.io";
+        $to = "vvishn405@gmail.com";
         $subject = "This is subject";
 
         $message = "<b>This is HTML message.</b>";
         $message .= "<h1>This is headline.</h1>";
 
-        $header = "From: \r\n";
+        $header = "From:vvishn405@gmail.com \r\n";
 
         $header .= "MIME-Version: 1.0\r\n";
         $header .= "Content-type: text/html\r\n";
