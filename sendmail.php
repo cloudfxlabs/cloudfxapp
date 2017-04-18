1f2b01fd60bf8bc0a035d0c0c8f6268452933003
vishnucfx <vgunipati@cloudfxlabs.io>
2017-04-18 20:18:38 +0530

    mail

--- sendmail.php
+++ sendmail.php
@@ -1,11 +1,32 @@
 <?php
-$from="vishnu405@hotmail.com";
-$name=$_POST['name'];
-$email=$_POST['email'];
-$subject=$_POST['subject'];
-$message=$_POST['message'];
-mail($name, $email, $subject, $message,"Form:".$from);
+$name=$_POST["name"]."\n";
+// Multiple recipients
+$to = 'vvishnu405@gmail.com';
+$email = $_POST["email"];//'vvishnu405@gmail.com'; // note the comma
 
-print "your message sent :</br>$name</br>$email</br>$subject</br>$message</p>";
+// Subject
+$subject = $_POST["subject"];//'Birthday Reminders for August';
 
+// Message
+$message = $name.$_POST["message"];
+
+// To send HTML mail, the Content-type header must be set
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+$headers[]= 'From:' .$email. "\r\n";
+
+// Additional headers
+//$headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
+//$headers[] = 'From:<admin@cloudfxlabs.io>';
+//$headers[] = 'Cc: birthdayarchive@example.com';
+//$headers[] = 'Bcc: birthdaycheck@example.com';
+
+// Mail it
+if(mail($to, $subject, $message, implode("\r\n", $headers)))
+{
+echo 'Mail has sent successfully';}
+else {
+echo 'Connection Problem email not sent';
+  exit;
+}
 ?>
